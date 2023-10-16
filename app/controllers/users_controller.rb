1b0af98619a4465394335ea2485e6da92356f431
kaya <kaaya.xx.x@gmail.com>
2023-10-16 16:07:51 +0900

    ログアウト機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only:[:new, :create]
 
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to user_path(@user.id)
     else
       render :new
     end
@@ -11,6 +12,10 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
