c800ffdaba1077041635c7205ef77fb358432fa2
kaya <kaaya.xx.x@gmail.com>
2023-10-17 22:49:14 +0900

    画像投稿編集ページの実装

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   has_many :favorites, dependent: :destroy
-  has_many :users, through: :favorites, source: :user
+  belongs_to :user
   mount_uploader :image, ImageUploader
   validates :content, length: {minimum: 1}
   validates :image, presence: true
