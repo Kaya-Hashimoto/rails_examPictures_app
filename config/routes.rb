0a84a8a2677fc9258fd958e6978bddd43cf277fa
kaya <kaaya.xx.x@gmail.com>
2023-10-18 18:27:19 +0900

    お気に入り登録と解除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   resources :sessions, only:[:new, :create, :destroy]
-  #resources :favorites, only[:create, :destroy]
+  resources :favorites, only:[:create, :destroy]
   resources :users
   resources :pictures do
     collection do
